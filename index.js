f88b8851a51a703a366ce726562a914cd6b785f1
Frank Hassanabad <foreverlinux@yahoo.com>
2013-06-02 23:22:03 -0600

    updated package json to be better and updated the index to be more informative

--- index.js
+++ index.js
@@ -6,4 +6,4 @@ app.use(express.logger('dev'));
 app.use(express.static(__dirname + '/public'));
 app.listen(80);
 
-console.log('Listening on port 80');
+console.log('Listening on port 80, point your browser to http://localhost');
